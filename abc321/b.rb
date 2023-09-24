ff0c99f8174c3a4a34f6b52b08a7b3911853d066
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2023-09-24 13:00:17 +0000

    修正＆コメント追加。

--- abc321/b.rb
+++ abc321/b.rb
@@ -1,3 +1,4 @@
+# ３つのケースで失敗している。なぜこれではだめなのか。
 n,x = gets.split(" ").map(&:to_i)
 nums = gets.split(" ").map(&:to_i)
 
@@ -6,7 +7,7 @@ sorted_nums = nums.sort
 min_val = sorted_nums[0..sorted_nums.length-2].sum
 max_val = sorted_nums[1..sorted_nums.length-1].sum
 
-if sorted_nums[0] == x
+if min_val == x
   p 0
 elsif min_val < x && x <= max_val
   p x - min_val + sorted_nums[0]
