91d9fec95038c250949e50f0691e726d86313141
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2023-09-25 13:53:24 +0000

    319のaを解いた

---
+++ abc319/a.rb
@@ -0,0 +1,9 @@
+name = gets.chomp
+
+hash = {
+  "tourist": 3858, "ksun48": 3679, "Benq": 3658, "Um_nik": 3648,
+  "apiad": 3638, "Stonefeang": 3630, "ecnerwala": 3613, "mnbvmar": 3555,
+  "newbiedmy": 3516, "semiexp": 3481
+}
+
+p hash[name.to_sym]
